4b54a039bdfd5959ca8e11dd8924314191bbba10
uttusharma <utkarshstudent1@gmail.com>
2020-09-15 15:41:37 +0530

    Basic Flow done

---
+++ simulation/js/main.js
@@ -0,0 +1,48 @@
+function startTutorial() {
+  var next1 = document.getElementById("intro");
+  var playground  = document.getElementById("slide1");
+  next1.setAttribute("hidden", "true");
+  playground.removeAttribute("hidden");
+}
+/*
+            For Slide PUSH
+*/
+function gotoSlide2(){
+    var slide = document.getElementById("slide1");
+    var nextSlide = document.getElementById("slide2");
+    slide.setAttribute("hidden","true");
+    nextSlide.removeAttribute("hidden");
+}
+function pushC(){
+    var codeC = document.getElementById("pushC");
+    var codePy = document.getElementById("pushPy");
+    codePy.setAttribute("hidden","true");
+    codeC.removeAttribute("hidden");
+
+}function pushPy(){
+    var codeC = document.getElementById("pushC");
+    var codePy = document.getElementById("pushPy");
+    codeC.setAttribute("hidden","true");
+    codePy.removeAttribute("hidden");
+}
+/*
+            For Slide POP
+*/
+function gotoSlide3(){
+    var slide = document.getElementById("slide2");
+    var nextSlide = document.getElementById("slide3");
+    slide.setAttribute("hidden","true");
+    nextSlide.removeAttribute("hidden");
+}
+function popC(){
+    var codeC = document.getElementById("popC");
+    var codePy = document.getElementById("popPy");
+    codePy.setAttribute("hidden","true");
+    codeC.removeAttribute("hidden");
+
+}function popPy(){
+    var codeC = document.getElementById("popC");
+    var codePy = document.getElementById("popPy");
+    codeC.setAttribute("hidden","true");
+    codePy.removeAttribute("hidden");
+}
